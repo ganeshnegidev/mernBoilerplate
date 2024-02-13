4f139b7533ad77bef3d05f8800264c339f5d11f9
Ganesh Singh Negi <ganeshnegi003@gmail.com>
2024-02-13 19:41:46 +0530

    adddded

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,43 @@
-// require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
+import http from 'http';
+import { WebSocketServer } from 'ws';
+
+
+const server = http.createServer(app);
 
 dotenv.config({
     path: './env'
 })
 
+const wss = new WebSocketServer({server});
+
+// WebSocket event handling
+wss.on('connection', (ws) => {
+    console.log('A new client connected.');
+  
+    // Event listener for incoming messages
+    ws.on('message', (message) => {
+      console.log('Received message:', message.toString());
+  
+      // Broadcast the message to all connected clients
+      wss.clients.forEach((client) => {
+        if (client.readyState === wss.OPEN) {
+          client.send(message.toString());
+        }
+      });
+    });
+  
+    // Event listener for client disconnection
+    ws.on('close', () => {
+      console.log('A client disconnected.');
+    });
+  });
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 5000, () => {
+    server.listen(process.env.PORT || 5000, () => {
         console.log(` Server is running at PORT : ${process.env.PORT}`);
     })
 })
--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,8 @@
-import React , {useState} from 'react'
+import React , {useEffect, useState} from 'react'
 import { clearuser } from '../../Store/UserSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { createTodo, removeTodo, updateTodo } from '../../Store/TodoSlice';
+let ws;
 
 export default function Dashboard() {
 
@@ -24,9 +25,16 @@ export default function Dashboard() {
         'company': company
     }
     dispatch(createTodo(data));
+    setEmail('');
+    setFName('');
+    setLName('');
+    setPhone('');
+    setCompany('');
+    const dataSoc = {
+        user: fName+''+lName
+    }
+    ws.send("created", JSON.stringify(dataSoc));
   }
-
-  console.log(todo);
 
   const editContent = (id) => {
     const data = {
@@ -39,6 +47,17 @@ export default function Dashboard() {
     dispatch(updateTodo(id,data));
   }
 
+  useEffect(() => {
+    socketInitiaze();
+  },[]);
+
+  const socketInitiaze = () => {
+    ws = new WebSocket('ws://localhost:5000/socket');
+    ws.onmessage = function (message) {
+      console.log('New notification: ' + message.data)
+    };
+  }
+
   return (
     <>
       <div className='w-full flex flex-row justify-between mx-auto'>
@@ -47,81 +66,80 @@ export default function Dashboard() {
       </div>
       <div className='flex flex-col items-center p-[100px] w-full'>
         <div className='font-bold'>To Do App</div>
-         <form onSubmit={HandleSave}>
+         <form>
             <div className="relative z-0 w-full mb-5 group">
                 <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
             </div>
             <div className="grid md:grid-cols-2 md:gap-6">
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="text" onChange={(e) => setFName(e.target.value)}  name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="text" value={fName} onChange={(e) => setFName(e.target.value)}  name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="text"  onChange={(e) => setLName(e.target.value)} name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="text" value={lName} onChange={(e) => setLName(e.target.value)} name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
                 </div>
             </div>
             <div className="grid md:grid-cols-2 md:gap-6">
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="tel" onChange={(e) => setPhone(e.target.value)} name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="tel"  value={phone} onChange={(e) => setPhone(e.target.value)} name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="text"  onChange={(e) => setCompany(e.target.value)} name="floating_company" id="floating_company" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="text" value={company} onChange={(e) => setCompany(e.target.value)} name="floating_company" id="floating_company" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
                 </div>
             </div>
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            <button type="button" onClick={HandleSave} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </form>
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" className="px-6 py-3">
-                            Company
+                          Company
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            Full Name
+                          Full Name
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            Email
+                          Email
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            Phone
+                          Phone
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            Action
+                          Action
                         </th>
                     </tr>
                     </thead>
                     <tbody>
                     {
-                        todo?.todos.map((item,index) => (
-                            <tr key={index} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                  {item.company}
-                                </th>
-                                <td className="px-6 py-4">
-                                  {item.fName} {item.lName}
-                                </td>
-                                <td className="px-6 py-4">
-                                  {item.email}
-                                </td>
-                                <td className="px-6 py-4">
-                                 {item.phone}
-                                </td>
-                                <td className="px-6 py-4">
-                                    {/* <a href="#" onClick={editContent}  className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
-                                    <a href="#" onClick={() => dispatch(removeTodo(item.id))} className="font-medium text-blue-600 dark:text-red-500 hover:underline ml-2">Delete</a>
-                                </td>
-                            </tr> 
+                      todo?.todos.map((item,index) => (
+                        <tr key={index} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                           <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                             {item.company}
+                           </th>
+                           <td className="px-6 py-4">
+                             {item.fName} {item.lName}
+                           </td>
+                           <td className="px-6 py-4">
+                             {item.email}
+                           </td>
+                           <td className="px-6 py-4">
+                             {item.phone}
+                           </td>
+                           <td className="px-6 py-4">
+                             {/* <a href="#" onClick={editContent}  className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> */}
+                             <button onClick={() => dispatch(removeTodo(item.id))} className="font-medium text-blue-600 dark:text-red-500 hover:underline ml-2">Delete</button>
+                           </td>
+                         </tr> 
                         ))
                     }                      
                     </tbody>
             </table>
         </div>
-
       </div>
     </>
   )
